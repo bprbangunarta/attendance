a0ad0c19a0773987edea632a00b17d0934564b19
Zulfadli Rizal <dev@bprbangunarta.co.id>
2024-01-04 19:26:02 +0700

    Fix

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AttendanceController extends Controller
 {
-    // use ImageStorage;
+    use ImageStorage;
 
     /**
      * Store presence status
